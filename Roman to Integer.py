87433291733e304520d62c3a34331fe7979ce033
Husnain <Husnain.tayab@hotmail.com>
2026-06-20 23:21:06 +0500

    Roman to Integer

--- Roman to Integer.py
+++ Roman to Integer.py
@@ -1,6 +1,6 @@
 class Solution:
     def romanToInt(self, s, ):
-        
+
         roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                  'C': 100, 'D': 500, 'M': 1000}
         result = 0
@@ -9,4 +9,5 @@ class Solution:
                 result -= roman[s[i]]
             else:
                 result += roman[s[i]]
+                
         return result
